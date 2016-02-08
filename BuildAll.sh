c09912f380b7be0d297de11d7dcb4ff84347c082
Joeri van Dooren <ure@moreorless.be>
2016-02-08 20:29:41 +0100

    fixes

--- BuildAll.sh
+++ BuildAll.sh
@@ -11,3 +11,5 @@ oc create -f hhvm/DeploymentConfig.yaml
 oc create -f hhvm/Services.yaml
 oc create -f hhvm/Route.yaml
 oc start-build hhvm
+
+oc process -f MysqlTemplate.yaml -v MYSQL_DATABASE=wordpress-hhvm,VOLUME_CAPACITY=512Mi | oc create -f -
