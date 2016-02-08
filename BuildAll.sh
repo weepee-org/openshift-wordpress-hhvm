878c873e75defaaff78acede1b701b3de1d726b0
Joeri van Dooren <ure@moreorless.be>
2016-02-08 20:42:01 +0100

    move

--- BuildAll.sh
+++ BuildAll.sh
@@ -4,6 +4,10 @@ oc new-project wordpress-hhvm \
     --description="WordPress HHVM" \
     --display-name="WordPress HHVM"
 
+oc create -f Gluster-Service.yaml
+oc create -f Gluster-Endpoint.yaml
+oc create -f PersistentVolumeClaim.yaml
+
 echo "hhvm"
 oc create -f hhvm/BuildConfig.yaml
 oc create -f hhvm/ImageStream.yaml
@@ -12,4 +16,5 @@ oc create -f hhvm/Services.yaml
 oc create -f hhvm/Route.yaml
 oc start-build hhvm
 
+echo "mysql"
 oc process -f MysqlTemplate.yaml -v MYSQL_DATABASE=wordpress-hhvm,VOLUME_CAPACITY=512Mi | oc create -f -
--- GetLatest.sh
+++ GetLatest.sh
@@ -4,4 +4,4 @@ rm -f latest.tar.gz > /dev/null 2>&1
 wget https://wordpress.org/latest.tar.gz
 tar zxvf latest.tar.gz
 echo latest.tar.gz > .gitignore
-mv wordpress hhvm/app
+mv wordpress hhvm/
